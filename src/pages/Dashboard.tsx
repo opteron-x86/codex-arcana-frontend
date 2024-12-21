ae7d49ed03e0be0de7611192db81722be17fad35
opteron-x86 <opteronx86@protonmail.com>
2024-12-21 14:13:51 -0500

    removed schema module

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import type { Schema } from "../../amplify/data/resource";
+
 import { useAuthenticator } from '@aws-amplify/ui-react';
 import { generateClient } from "aws-amplify/data";
 import { Plus, Trash2 } from 'lucide-react';
