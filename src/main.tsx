09cbeed6896705e93f960baf4a52a2b9e4a01fe1
opteron-x86 <opteronx86@protonmail.com>
2024-12-23 16:50:16 -0500

    updated for HTTP API

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import { AuthProvider } from 'react-oidc-context';
 import App from './App';
 
 const cognitoAuthConfig = {
-  authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_vx4KtfYEy",
-  client_id: '3t6ps7f32n947q67ovi1ikarjg',
+  authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_mLBO8uCgR",
+  client_id: '3iaac8pddtpirbst142eb7jeg1',
   redirect_uri: 'https://dev.d29017v31a8sv8.amplifyapp.com/', // or your production URL
   response_type: 'code',
   scope: 'openid email profile', 
