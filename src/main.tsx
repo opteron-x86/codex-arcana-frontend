f1fcc98d8006ad7381b16ca25ed02456a1719ed9
opteron-x86 <opteronx86@protonmail.com>
2024-12-22 19:58:27 -0500

    refactor for cognito

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,17 @@ import App from './App';
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_vx4KtfYEy",
   client_id: '3t6ps7f32n947q67ovi1ikarjg',
-  redirect_uri: 'http://localhost:3000', // or your production URL
+  redirect_uri: 'https://d84l1y8p4kdic.cloudfront.net', // or your production URL
   response_type: 'code',
   scope: 'openid email profile', 
 };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+if (!container) {
+  throw new Error("Could not find 'root' element in the DOM");
+}
+
+const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
     <AuthProvider {...cognitoAuthConfig}>
