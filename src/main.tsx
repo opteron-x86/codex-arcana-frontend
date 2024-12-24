ac006dfdb83c114a55e39a2a504b105b50607162
opteron-x86 <opteronx86@protonmail.com>
2024-12-23 23:05:26 -0500

    fixed for env variables

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ const cognitoAuthConfig = {
   authority: process.env.REACT_APP_COGNITO_AUTHORITY,
   client_id: process.env.REACT_APP_COGNITO_CLIENT_ID,
   redirect_uri: process.env.REACT_APP_COGNITO_REDIRECT_URI,
-  response_type: "code", // Typically fixed
+  response_type: "code",
   scope: process.env.REACT_APP_COGNITO_SCOPE,
 };
 
